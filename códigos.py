4bc00decc8e9d21422761233b06592c0eddef8c3
devNanteS <l.nantes@aluno.ifsp.edu.br>
2024-08-07 11:57:04 -0300

    Add files via upload

---
+++ códigos.py
@@ -0,0 +1,5 @@
+n1 = float(input("Coloque a primeira nota : "))
+n2 = float(input("Coloque a segunda nota : "))
+n3 = float(input("Coloque a terceira nota : "))
+m = (n1 + n2 + n3)/3
+print("A sua média é : ", '%2.f' %m )
---
+++ script.js
@@ -0,0 +1,7 @@
+//criando variável
+let mensagem1 = "Seja bem vindo ao meu tutorial de ";
+let mensagem2 = "JavaScript";
+
+//mostrando o resultado
+console.log (mensagem1);
+console.log (mensagem2);
